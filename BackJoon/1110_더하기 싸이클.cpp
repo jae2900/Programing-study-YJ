ba62134604c51027a2e818accbf9f1a9a2bc120b
jae2900 <dldudwo78951@naver.com>
2021-01-01 02:44:27 +0900

    1110_더하기 싸이클 문제풀이 B1

---
+++ BackJoon/1110_더하기 싸이클.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void sol()
+{
+    int num, n1,n2,n3=0,temp,cnt = 0;
+
+    cin >> num;
+    temp = num;
+    while(1)
+    {
+       
+        
+        if(temp<10)
+            n1 = 0;
+        else
+            n1=temp/10;
+        n2=temp%10;
+        n3=(n1+n2)%10;
+        temp = n2*10+n3;
+        cnt++;
+        if(temp == num)
+            break;
+    }
+    cout << cnt <<endl;
+}
+
+int main()
+{
+    sol();
+    return 0;
+}
