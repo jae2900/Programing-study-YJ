1d2126dc0a3522f3f6bf500aa3cb21fc1ff772ce
jae2900 <dldudwo78951@naver.com>
2021-01-07 17:37:33 +0900

    Programmers 가운데 글자 가져오기 문제풀이 L1

---
+++ Programmers/PG_가운데 글자 가져오기_L1.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+string temp;
+
+string solution(string s)
+{
+	string answer = "";
+
+	if (s.size() % 2 == 0)
+	{
+		answer.push_back(s[s.size() / 2-1]);
+		answer.push_back(s[s.size() / 2]);
+	}
+	else
+	{
+		answer.push_back(s[s.size() / 2]);
+	}
+
+
+
+	return answer;
+}
