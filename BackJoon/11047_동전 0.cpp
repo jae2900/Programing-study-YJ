96c946b863284ce28525ba6634f0a41d24ee0b78
jae2900 <dldudwo78951@naver.com>
2021-01-13 18:50:55 +0900

    BackJoon 11047_동전 0 문제 풀이 S1

---
+++ BackJoon/11047_동전 0.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solution()
+{
+	int N, K, answer = 0;
+	vector <int> value;
+	int x;
+	cin >> N >> K;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x;
+		value.push_back(x);
+	}
+	int cnt = value.size() - 1;
+	while (1)
+	{
+		if (K >= value.at(cnt))
+		{
+			K -= value.at(cnt);
+			answer++;
+		}
+		else
+			cnt--;
+		if (K == 0)
+			break;
+
+	}
+	cout << answer << endl;
+
+}
+
+int main()
+{
+	solution();
+
+	return 0;
+}
