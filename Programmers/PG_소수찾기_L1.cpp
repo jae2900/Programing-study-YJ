18ee1085536c14d0dcc7f3247f40221f108b1d79
jae2900 <dldudwo78951@naver.com>
2021-01-24 22:52:00 +0900

    Programmers 소수찾기 문제풀이 L1 완전 탐색을 사용했으나 효율성 문제로 실패한 케이스

---
+++ Programmers/PG_소수찾기_L1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+bool check(int a)
+{
+
+	for (int j = 2; j < a; j++)
+	{
+		if (a%j == 0)
+		{
+			return false;
+		}
+
+	}
+	return true;
+}
+
+int solution(int n) {
+	int answer = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		if (i % 2 != 0)
+		{
+			if (check(i))
+				answer++;
+		}
+		else if (i == 2)
+			answer++;
+			
+		
+
+	}
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution(10);
+	return 0;
+}
