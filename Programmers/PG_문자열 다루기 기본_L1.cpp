4cef014961acedd5fa0c95f8584b7615f8cad402
jae2900 <dldudwo78951@naver.com>
2021-01-29 21:06:53 +0900

    no message

--- Programmers/PG_문자열 다루기 기본_L1.cpp
+++ Programmers/PG_문자열 다루기 기본_L1.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-bool solution(string s) {
+bool solution(string s) 
+{
+	// 아스키 코드의 범위를 이용하여 숫자인지 판별
+
     bool answer = true;
     if(s.size() != 4 && s.size() != 6)
         answer = false;
