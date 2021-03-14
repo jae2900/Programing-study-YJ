73091cfd75fd409a4bceaeda15bb928b1cefa846
jae2900 <dldudwo78951@naver.com>
2021-03-14 15:49:11 +0900

    BackJoon 2577 숫자의 개수 문제 풀이 B2

---
+++ BackJoon/2577_숫자의 개수_V2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+ 
+int main(void){
+    int a, b, c;        //입력받을 3가지 변수 
+    int arr[10] = {0};    //0으로 초기화 
+    
+    cin >> a >> b >> c;    // 숫자 3가지를 받습니다. 
+    
+    int result = a*b*c;
+    
+    //1의 자리를 얻어 옵니다.
+    //resutl 가 0 이 되면 while문을 빠져나갑니다. 
+    while(result != 0){
+        arr[result % 10] +=1;
+        result /= 10;
+    }
+    
+    //출력. 
+    for(int i=0; i<10; i++){
+        cout << arr[i] << endl;
+    }
+    
+    return 0;
+}
+
+
