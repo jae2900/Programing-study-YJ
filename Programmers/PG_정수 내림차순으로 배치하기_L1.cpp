06be9d52534741590f0897d620c01bc8bb70e954
jae2900 <dldudwo78951@naver.com>
2021-04-06 22:26:58 +0900

    Programmers 정수 내림차순으로 배치하기 Code 작성중

---
+++ Programmers/PG_정수 내림차순으로 배치하기_L1.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    vector <int> num;
+    while(n != 0)
+    {
+        
+    }
+
+    return answer;
+}
